792ea91d4db9293f50439fdfe5f2966a455a2680
htgarym <garym@hightouchinc.com>
2024-02-12 21:31:05 -0600

    Did stuff

--- app/src/main/kotlin/dev/garymyers/logging/vector/App.kt
+++ app/src/main/kotlin/dev/garymyers/logging/vector/App.kt
@@ -7,10 +7,12 @@ fun main() {
     val logger = LoggerFactory.getLogger("Test Logger")
     val faker = Faker()
 
-    logger.trace("{}", faker.ancient().hero())
-    logger.debug("{}", faker.aquaTeenHungerForce().character())
-    logger.info("{}", faker.backToTheFuture().quote())
-    logger.warn("{}", faker.beer().name())
-    logger.error("{}", faker.app().name())
-    logger.error("Exception", Throwable(faker.book().author()))
+    (0..10).forEach { _ ->
+        logger.trace("{}", faker.ancient().hero())
+        logger.debug("{}", faker.aquaTeenHungerForce().character())
+        logger.info("{}", faker.backToTheFuture().quote())
+        logger.warn("{}", faker.beer().name())
+        logger.error("{}", faker.app().name())
+        logger.error("Exception", Throwable(faker.book().author()))
+    }
 }
